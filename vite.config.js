3d625fd35dbf056315f566032f243c1298afecba
unknown <natnaelsintayehu5@gmail.com>
2025-08-27 12:13:58 -0700

    Initial commit with docs build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/brook/",
+  base: "/brook/", // 👈 repo name (important for GitHub Pages)
+  build: {
+    outDir: "docs", // GitHub Pages expects /docs in main branch
+  },
 });
